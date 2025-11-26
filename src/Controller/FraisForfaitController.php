f0882e4240145fb7d66c0393eb601e969696ab49
Julien <Julienmichelin35@gmail.com>
2025-11-27 00:27:51 +0100

    Ajout de la fonctionnalité permettant de vérifier si un FF existe déjà

--- src/Controller/FraisForfaitController.php
+++ src/Controller/FraisForfaitController.php
@@ -43,22 +43,47 @@ final class FraisForfaitController extends AbstractController
     }
 
     #[Route('/fraisforfaits', name: 'fraisforfaits_post', methods: ['POST'])]
-    function createFraisForfait(Request $request, SerializerInterface $unSerialiseur, EntityManagerInterface $em, URLGeneratorInterface $unUrlGenerateur)
+    public function createFraisForfait(Request $request,FraisForfaitRepository $unFraisForfaitRepository, SerializerInterface $unSerialiseur, EntityManagerInterface $em, URLGeneratorInterface $unUrlGenerateur)
     {
         $contenu = $request->getContent();
         $unFraisForfait = $unSerialiseur->deserialize($contenu, FraisForfait::class, 'json');
         $em->persist($unFraisForfait);
-        $em->flush();
+        if ($this->dejaPresent($contenu, $unFraisForfaitRepository, $unSerialiseur)){
+            $em->flush();
+                $location = $unUrlGenerateur->generate('fraisforfaits_post', 
+                                        ['id' => $unFraisForfait->getId()],
+                                        UrlGeneratorInterface::ABSOLUTE_URL);
 
-        $location = $unUrlGenerateur->generate('fraisforfaits_post', 
-                                ['id' => $unFraisForfait->getId()],
-                                UrlGeneratorInterface::ABSOLUTE_URL);
+                $result = ["message" => "Nouveau frais forfait créé",
+                        "data" => [
+                            "_selfLink" => $location
+                            ]
+                        ];
+                return new JsonResponse($result, JSONResponse::HTTP_CREATED, [], false);
+        }
+        else {
+            $result = ["message" => "Id frais forfait déjà existant"];
+            return new JsonResponse($result, JSONResponse::HTTP_CONFLICT, [], false);
+        }
+    }
 
-        $result = ["message" => "Nouveau frais forfait créé",
-                   "data" => [
-                    "_selfLink" => $location
-                    ]
-                ];
-        return new JsonResponse($result, JSONResponse::HTTP_CREATED, [], false);
+    /**
+     * Renvoie un booleen en fonction de si l'id est dékà implémenter dans la bdd
+     * @param string $contenu le contenue récupérer de la requête
+     * @param FraisForfaitRepository $unFraisForfaitRepository
+     * @param SerializerInterface $unSerialiseur
+     * @return bool Renvoie true si l'id est déjà present, sinon false
+     */
+    public function dejaPresent(string $contenu, FraisForfaitRepository $unFraisForfaitRepository, SerializerInterface $unSerialiseur): bool{
+        $idStart = 10; // a l'entrée de la guillemet
+        $idEnd = strpos($contenu, ',') - 1; // position avant la guillemet
+        $allFF = $this->getDetailFraisForfait(substr($contenu, $idStart, $idEnd - $idStart), $unFraisForfaitRepository, $unSerialiseur);
+        $rep = json_decode($allFF->getContent(), true);
+        if ($rep['message'] === 'Id frais forfait inexistant'){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
