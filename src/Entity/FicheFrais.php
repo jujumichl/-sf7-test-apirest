0e32582a88aae7bccbc90da6dc30b7939bc5d878
Julien MICHELIN <julien.michelin@lycee-basch.fr>
2025-12-01 17:47:55 +0100

    Création de Visiteur, Etat & FicheFrais

---
+++ src/Entity/FicheFrais.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FicheFraisRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FicheFraisRepository::class)]
+class FicheFrais
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 6)]
+    private ?string $mois = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $nbJustificatifs = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $montantValide = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $dateModif = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Visiteur $visiteur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMois(): ?string
+    {
+        return $this->mois;
+    }
+
+    public function setMois(string $mois): static
+    {
+        $this->mois = $mois;
+
+        return $this;
+    }
+
+    public function getNbJustificatifs(): ?int
+    {
+        return $this->nbJustificatifs;
+    }
+
+    public function setNbJustificatifs(int $nbJustificatifs): static
+    {
+        $this->nbJustificatifs = $nbJustificatifs;
+
+        return $this;
+    }
+
+    public function getMontantValide(): ?string
+    {
+        return $this->montantValide;
+    }
+
+    public function setMontantValide(string $montantValide): static
+    {
+        $this->montantValide = $montantValide;
+
+        return $this;
+    }
+
+    public function getDateModif(): ?\DateTime
+    {
+        return $this->dateModif;
+    }
+
+    public function setDateModif(\DateTime $dateModif): static
+    {
+        $this->dateModif = $dateModif;
+
+        return $this;
+    }
+
+    public function getVisiteur(): ?Visiteur
+    {
+        return $this->visiteur;
+    }
+
+    public function setVisiteur(?Visiteur $visiteur): static
+    {
+        $this->visiteur = $visiteur;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Visiteur.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VisiteurRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VisiteurRepository::class)]
+class Visiteur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'NONE')]
+    #[ORM\Column(length: 4)]
+    private ?string $id = null;
+
+    #[ORM\Column(length: 30, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 30, nullable: true)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $login = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $mdp = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 5, nullable: true)]
+    private ?string $cp = null;
+
+    #[ORM\Column(length: 30, nullable: true)]
+    private ?string $ville = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $dateEmbauche = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): static
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getMdp(): ?string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(string $mdp): static
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getCp(): ?string
+    {
+        return $this->cp;
+    }
+
+    public function setCp(?string $cp): static
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(?string $ville): static
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getDateEmbauche(): ?\DateTime
+    {
+        return $this->dateEmbauche;
+    }
+
+    public function setDateEmbauche(\DateTime $dateEmbauche): static
+    {
+        $this->dateEmbauche = $dateEmbauche;
+
+        return $this;
+    }
+}
